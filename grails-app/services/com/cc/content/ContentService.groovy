32b5f605ee961387d5f25c902c21696f9ae40887
Shashank Agrawal <shashank.agrawal@causecode.com>
2013-05-16 16:28:54 +0530

    Modified createlink functionality moved to service. #4

--- grails-app/services/com/cc/content/ContentService.groovy
+++ grails-app/services/com/cc/content/ContentService.groovy
@@ -8,10 +8,18 @@
 
 package com.cc.content
 
+import java.lang.annotation.Annotation
+import java.lang.reflect.Field
+
 import org.codehaus.groovy.grails.commons.metaclass.GroovyDynamicMethodsInterceptor
 import org.codehaus.groovy.grails.plugins.springsecurity.SpringSecurityUtils
+import org.codehaus.groovy.grails.web.mapping.LinkGenerator
 import org.codehaus.groovy.grails.web.metaclass.BindDynamicMethod
+import org.codehaus.groovy.grails.web.servlet.mvc.GrailsWebRequest
+import org.springframework.beans.factory.annotation.Autowired
 
+import com.cc.annotation.sanitizedTitle.SanitizedTitle
+import com.cc.annotation.shorthand.ControllerShorthand
 import com.cc.blog.Blog
 import com.cc.content.meta.Meta
 import com.cc.page.Page
@@ -20,8 +28,12 @@ class ContentService {
 
     private static final String ANONYMOUS_USER = "anonymousUser"
 
+    def friendlyUrlService
     def grailsApplication
     def springSecurityService
+
+    @Autowired
+    LinkGenerator linkGenerator
 
     ContentService() {
         GroovyDynamicMethodsInterceptor i = new GroovyDynamicMethodsInterceptor(this)
@@ -104,6 +116,74 @@ class ContentService {
             }
         }
         return contentInstance
+    }
+
+    String createLink(Map attrs, request, response) {
+        if(!attrs.domain)
+            return
+
+        boolean useJsessionId = grailsApplication.config.grails.views.enable.jsessionid
+
+        Class domainClass, controllerClass
+        domainClass = grailsApplication.getDomainClass(attrs.domain).clazz
+        def domainClassInstance = domainClass.get(attrs.id)     // Get actual domainInstance
+        List<Field> fields = []
+
+        if(domainClass.superclass && domainClass.superclass != Object)
+            fields.addAll(domainClass.superclass.getDeclaredFields())   // Adding fields from super class
+        fields.addAll(domainClass.getDeclaredFields())      // Adding fields from current class
+
+        String fieldName, sanitizedTitle, controllerShortHand = ""
+        for(field in fields) {
+            if(field.isAnnotationPresent(SanitizedTitle.class) && field.type == String) {  // Searching annotation on each field
+                fieldName = field.name
+                break
+            }
+        }
+        if(fieldName && domainClassInstance)
+            sanitizedTitle = friendlyUrlService.sanitizeWithDashes(domainClassInstance[fieldName])
+        else
+            log.error "No annotated field found in domain class ${domainClassInstance?.class}"
+
+        for(controller in grailsApplication.controllerClasses) {
+            if(controller.name == attrs.controller.capitalize()) {
+                controllerClass = controller.clazz
+            }
+        }
+
+        Annotation controllerAnnotation = controllerClass?.getAnnotation(ControllerShorthand.class)
+        if(controllerAnnotation) {  // Searching for shorthand for grails controller
+            controllerShortHand = controllerAnnotation.value()
+        }
+
+        if(controllerShortHand)
+            attrs.uri = "/$controllerShortHand/${attrs.id}/${sanitizedTitle ?: ''}"
+        else
+            log.error "No annotation found for controller: ${controllerClass.class}"
+
+        def urlAttrs = attrs
+        if (attrs.url instanceof Map) {
+            urlAttrs = attrs.url
+        }
+        def params = urlAttrs.params && urlAttrs.params instanceof Map ? urlAttrs.params : [:]
+        if (request['flowExecutionKey']) {
+            params."execution" = request['flowExecutionKey']
+            urlAttrs.params = params
+            if (attrs.controller == null && attrs.action == null && attrs.url == null && attrs.uri == null) {
+                urlAttrs[LinkGenerator.ATTRIBUTE_ACTION] = GrailsWebRequest.lookup().actionName
+            }
+        }
+        if (urlAttrs.event) {
+            params."_eventId" = urlAttrs.remove('event')
+            urlAttrs.params = params
+        }
+        def generatedLink = linkGenerator.link(attrs, request.characterEncoding)
+
+        if (useJsessionId) {
+            return response.encodeURL(generatedLink)
+        } else {
+            return generatedLink
+        }
     }
 
 }
--- grails-app/taglib/com/cc/content/ContentTagLib.groovy
+++ grails-app/taglib/com/cc/content/ContentTagLib.groovy
@@ -8,24 +8,11 @@
 
 package com.cc.content
 
-import java.lang.reflect.Field
-
-import org.codehaus.groovy.grails.web.mapping.LinkGenerator
-import org.codehaus.groovy.grails.web.servlet.mvc.GrailsWebRequest
-import org.springframework.beans.factory.annotation.Autowired
-
-import com.cc.annotation.sanitizedTitle.SanitizedTitle;
-import com.cc.annotation.shorthand.ControllerShorthand;
-
 class ContentTagLib {
 
     static namespace = "content"
 
-    @Autowired
-    LinkGenerator linkGenerator
-
     def contentService
-    def friendlyUrlService
 
     /**
      * Used when current user have content role
@@ -55,69 +42,10 @@ class ContentTagLib {
     }
 
     def createLink = { attrs, body ->
-        boolean useJsessionId = grailsApplication.config.grails.views.enable.jsessionid
         if(!attrs.domain)
             throwTagError("Tag content:createLink missing required attribute domain")
 
-        Class domainClass = grailsApplication.getDomainClass(attrs.domain).clazz
-        def domainClassInstance = domainClass.get(attrs.id)     // Get actual domainInstance
-        List<Field> fields = []
-
-        if(domainClass.superclass && domainClass.superclass != Object)
-            fields.addAll(domainClass.superclass.getDeclaredFields())   // Adding fields from super class
-        fields.addAll(domainClass.getDeclaredFields())
-
-        String fieldName, sanitizedTitle, controllerShortHand = ""
-        for(field in fields) {
-            if(field.isAnnotationPresent(SanitizedTitle.class) && field.type == String) {  // Searching annotation on each field
-                fieldName = field.name
-                break
-            }
-        }
-        if(fieldName)
-            sanitizedTitle = friendlyUrlService.sanitizeWithDashes(domainClassInstance[fieldName])
-        else
-            log.error "No annotated field found in domain class ${domainClassInstance.class}"
-
-        Class controllerClass
-        for(controller in grailsApplication.controllerClasses) {
-            if(controller.name == attrs.controller.capitalize()) {
-                controllerClass = controller.clazz
-            }
-        }
-
-        def controllerAnnotation = controllerClass?.getAnnotation(ControllerShorthand.class)
-        if(controllerAnnotation) {
-            controllerShortHand = controllerAnnotation.value()
-        }
-
-        if(controllerShortHand)
-            attrs.uri = "/$controllerShortHand/${attrs.id}/${sanitizedTitle ?: ''}"
-        else
-            log.error "No annotation found for controller: ${controllerClass.class}"
-        def urlAttrs = attrs
-        if (attrs.url instanceof Map) {
-            urlAttrs = attrs.url
-        }
-        def params = urlAttrs.params && urlAttrs.params instanceof Map ? urlAttrs.params : [:]
-        if (request['flowExecutionKey']) {
-            params."execution" = request['flowExecutionKey']
-            urlAttrs.params = params
-            if (attrs.controller == null && attrs.action == null && attrs.url == null && attrs.uri == null) {
-                urlAttrs[LinkGenerator.ATTRIBUTE_ACTION] = GrailsWebRequest.lookup().actionName
-            }
-        }
-        if (urlAttrs.event) {
-            params."_eventId" = urlAttrs.remove('event')
-            urlAttrs.params = params
-        }
-        def generatedLink = linkGenerator.link(attrs, request.characterEncoding)
-
-        if (useJsessionId) {
-            return response.encodeURL(generatedLink)
-        } else {
-            out << generatedLink
-        }
+        out << contentService.createLink(attrs, request, response)
     }
 
 }
