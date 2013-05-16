ea13a9c40aa3da4b8a94129a06a51829406e888c
Shashank Agrawal <shashank.agrawal@causecode.com>
2013-05-16 10:00:17 +0530

    Friendly url service from Altruhelp repo. https://github.com/causecode/altruhelp/issues/479

---
+++ test/unit/com/cc/seo/friendlyurl/FriendlyUrlServiceTests.groovy
@@ -0,0 +1,17 @@
+package com.cc.seo.friendlyurl
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
+ */
+@TestFor(FriendlyUrlService)
+class FriendlyUrlServiceTests {
+
+    void testSomething() {
+        fail "Implement me"
+    }
+}
