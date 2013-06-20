d536a76ee8dd23da27acd591e4aa763e1a50f62e
Shashank Agrawal <shashank.agrawal@causecode.com>
2013-06-20 15:15:06 +0530

    Plugin version 1.9 released.

--- ContentGrailsPlugin.groovy
+++ ContentGrailsPlugin.groovy
@@ -13,7 +13,7 @@ import com.cc.content.ContentService
 
 class ContentGrailsPlugin {
 
-    def version = "1.8"
+    def version = "1.9"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.1 > *"
     // the other plugins this plugin depends on
