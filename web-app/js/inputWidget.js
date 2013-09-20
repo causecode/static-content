4d7b7bdc94ca6189da839d82984a515e6e0a116a
Laxmi Salunkhe <laxmi.salunkhe@causecode.com>
2013-09-20 06:43:23 +0530

    Updated input widget domain structure and form template.

--- web-app/js/inputWidget.js
+++ web-app/js/inputWidget.js
@@ -1,48 +1,24 @@
-$("select.inputWidgetName").change(function(){
-    var str = "";
-    $( "select.inputWidgetName option:selected" ).each(function() {
-        str += $( this ).text() + " ";
-      });
-    if(str == 'select ') {
-        $(".inputWidgetType").hide();
-        $("select#inputWidgetSelect").show();
-        $("select#inputWidgetSelect").data('selected','true');
-        $("div#widgetKey").show();
-        $("div#widgetValue").show();
-        
-    } else if(str == 'checkBox ') {
-        //$(".inputWidgetType").hide();
-        $("input#inputWidgetCheckbox").show();
-        $("div#widgetKey").hide();
-        $("div#widgetValue").hide();
-        $("input#inputWidgetCheckbox").data('selected','true');
-        $("input[type='text']#defaultValue").change(function(){
-            if($( this ).val())
-            $( "input#inputWidgetCheckbox" ).click();
-        });
-    } else if(str == 'textField ') {
-        $(".inputWidgetType").hide();
-        $("input#inputWidgetTextfield").show();
-        $("input#inputWidgetTextfield").data('selected','true');
-        $("div#widgetKey").hide();
-        $("div#widgetValue").hide();
-        $("input[type='text']#defaultValue").change(function(){
-            $( "input#inputWidgetTextfield" ).val( $( this ).val() );
-        });
-    } else if(str == 'textArea ') {
-        $(".inputWidgetType").hide();
-        $("textarea#inputWidgetTextarea").show();
-        $("textarea#inputWidgetTextarea").data('selected','true');
-        $("div#widgetKey").hide();
-        $("div#widgetValue").hide();
-        $("input[type='text']#defaultValue").change(function(){
-            $( "textarea#inputWidgetTextarea" ).text( $( this ).val() );
-        });
-    }
-});
+$("#type").on("change", function(){
+    $("div.minChar").addClass("hide");
+    $("div.maxChar").addClass("hide");
+    $("div.minValueRange").addClass("hide");
+    $("div.maxValueRange").addClass("hide");
+    $("div.widgetKeyValue").addClass("hide");
+    $("div.noSelected").addClass("hide");
+    $("div.defaultSelected").addClass("hide");
 
-$("input[type='text']#label").change(function(){
-    var str = "";
-    str = $( this ).val() + "  :  ";
-    $( "td#label" ).text( str );
+    var $type = $(this).val()
+    if($type == 'SELECT' || $type == 'MULTI_SELECT') {
+        $("div.widgetKeyValue").removeClass("hide");
+        $("div.noSelected").removeClass("hide");
+    }
+    if($type == 'CHECKBOX' || $type == 'SELECT' || $type == 'MULTI_SELECT' || $type == 'RADIO') {
+        $("div.defaultSelected").removeClass("hide");
+    } 
+    if($type == 'TEXT_AREA' || $type == 'TEXT_FIELD') {
+        $("div.minChar").removeClass("hide");
+        $("div.maxChar").removeClass("hide");
+        $("div.minValueRange").removeClass("hide");
+        $("div.maxValueRange").removeClass("hide");
+    }
 });
