a2e506a112e45d4ac2e48d91b37d49e272f85699
Shashank Agrawal <shashank.agrawal@causecode.com>
2013-10-28 18:09:11 +0530

    Renamed resource files. #34

--- grails-app/conf/ContentResources.groovy
+++ grails-app/conf/ContentResources.groovy
@@ -1,16 +1,18 @@
 modules = {
     menuItem {
-        resource id:'js', url:[dir:'js/', file:"editMenuItemOrder.js"], disposition:'defer', minify: true
-        resource id:'css', url:[dir:'css/', file:"menuItem.css"], disposition:'defer', minify: true
+        resource id: 'js', url: [dir: 'js/', file: "menu-item-sorting.js"], minify: true
+        resource id: 'css', url: [dir: 'css/', file: "menu-item-sorting.css"], minify: true
         dependsOn 'draggableAndSortable'
     }
-    
+
     inputWidget {
-        resource id:'js', url:[dir:'js/', file:"inputWidget.js"], disposition:'defer', minify: true
+        resource id:'js', url:[dir:'js/', file:"inputWidget.js"], minify: true
     }
+
     draggableAndSortable {
         resource id: 'js', url: [dir: 'js/jquery-ui', file: 'jquery-ui-1.10.3.custom.js']
     }
+
     wordAndCharacterCounter {
         resource url: [dir: "js", file: "word-and-character-counter.js"]
     }
