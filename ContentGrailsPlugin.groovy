029a842cb199a9623ec4567e5863b05cb8cb6109
Shashank Agrawal <shashank.agrawal@causecode.com>
2014-01-14 17:21:34 +0530

    Release new plugin version 2.1.2

--- ContentGrailsPlugin.groovy
+++ ContentGrailsPlugin.groovy
@@ -15,7 +15,7 @@ import com.cc.content.ContentService
 
 class ContentGrailsPlugin {
 
-    def version = "2.1.1"
+    def version = "2.1.2"
     def groupId = "com.cc.plugins"
     def grailsVersion = "2.1 > *"
     def pluginExcludes = [
