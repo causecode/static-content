06e6ffcac843edb1fb4cc8c0459b84410af84b02
Laxmi Salunkhe <laxmi.salunkhe@causecode.com>
2014-08-27 09:38:29 +0530

    Fixed copyright message for blog marshaller groovy class.

--- src/groovy/com/cc/marshaller/BlogDomainMarshaller.groovy
+++ src/groovy/com/cc/marshaller/BlogDomainMarshaller.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011, Neighbor Marketing Inc 2014.
+ * Copyright (c) 2011, CauseCode Technologies Pvt Ltd, India.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or
