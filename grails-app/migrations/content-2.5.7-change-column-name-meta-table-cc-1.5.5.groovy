5866a6d039c19c73b867ebb4c27a60a4199513e9
Hardik Modha <hardik.modha@causecode.com>
2016-12-28 21:39:22 +0530

    Added migration to change column name.

---
+++ grails-app/migrations/content-2.5.7-change-column-name-meta-table-cc-1.5.5.groovy
@@ -0,0 +1,10 @@
+databaseChangeLog = {
+
+    changeSet(author: 'Hardik Modha', id: '28122016-01') {
+        grailsChange {
+            change {
+                sql.execute("ALTER TABLE `causecode`.`cc_content_meta` CHANGE COLUMN `value` `content` VARCHAR(255) NOT NULL;");
+            }
+        }
+    }
+}
