2489b1363b823a0e4b4a545eb2adb43247f48ef4
Hardik Modha <hardik.modha@causecode.com>
2016-12-29 13:28:15 +0530

    Removed database name from migration query.

--- grails-app/migrations/content-2.5.7-change-column-name-meta-table-cc-1.5.5.groovy
+++ grails-app/migrations/content-2.5.7-change-column-name-meta-table-cc-1.5.5.groovy
@@ -3,7 +3,7 @@ databaseChangeLog = {
     changeSet(author: 'Hardik Modha', id: '28122016-01') {
         grailsChange {
             change {
-                sql.execute("ALTER TABLE `causecode`.`cc_content_meta` CHANGE COLUMN `value` `content` VARCHAR(255) NOT NULL;");
+                sql.execute("ALTER TABLE `cc_content_meta` CHANGE COLUMN `value` `content` VARCHAR(255) NOT NULL;");
             }
         }
     }
