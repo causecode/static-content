7afc7497ee9ba506d735c3abe6053d992b02c102
Ankit Agrawal <ankit.agrawal@causecode.com>
2018-12-03 16:24:26 +0530

    Added configurations to publish plugin to nexus.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -10,6 +10,11 @@ grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
 
+grails.project.repos.ccRepo.url = "https://nexus.causecode.com/repository/maven-releases/"
+grails.project.repos.default = "ccRepo"
+grails.project.repos.ccRepo.type = "maven"
+
+grails.project.dependency.resolver = "maven"
 grails.project.dependency.resolution = {
     inherits("global") {
     }
@@ -19,24 +24,27 @@ grails.project.dependency.resolution = {
 		inherits true
 		grailsHome()
 		grailsPlugins()
-        mavenRepo "http://repo.spring.io/milestone/"
-        mavenRepo "http://maven.causecode.com"
         grailsCentral()
         mavenLocal()
         mavenCentral()
+        mavenRepo "http://repo.spring.io/milestone/"
+        mavenRepo "https://nexus.causecode.com/repository/maven-releases/"
     }
 
     dependencies {
     }
 
     plugins {
-        compile (":spring-security-core:2.0-RC4", ":taggable:1.0.1") {
+        compile (":spring-security-core:2.0-RC3", ":taggable:1.0.1") {
             export = false
         }
         runtime (":resources:1.2.1") {
             export = false
         }
         build (":tomcat:7.0.50", ":release:3.0.1", ":rest-client-builder:2.0.1") {
+            export = false
+        }
+        compile (":hibernate:3.6.10.7") {
             export = false
         }
     }
