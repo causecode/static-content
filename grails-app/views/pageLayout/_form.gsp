67c2981ccee2b6ffe4da1a7309f407babfce377f
Shivam Pandey <shivam.pandey@causecode.com>
2019-04-18 18:49:15 +0530

    removed the copyright statements and license addded

--- grails-app/views/pageLayout/_form.gsp
+++ grails-app/views/pageLayout/_form.gsp
@@ -1,9 +1,3 @@
-<!--  Copyright (c) 2011, CauseCode Technologies Pvt Ltd, India.
-All rights reserved.
-
-Redistribution and use in source and binary forms, with or without modification,
-are not permitted. -->
-
 <%@ page import="com.causecode.content.PageLayout"%>
 
 <div class="form-group ${hasErrors(bean: pageLayoutInstance, field: 'layoutName', 'error')} ">
--- src/integration-test/groovy/com/causecode/crm/BaseIntegrationTestCase.groovy
+++ src/integration-test/groovy/com/causecode/crm/BaseIntegrationTestCase.groovy
@@ -1,10 +1,4 @@
 /*
- * Copyright (c) 2011-Present, CauseCode Technologies Pvt Ltd, India.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or
- * without modification, are not permitted.
- *//*
 
 
 package com.causecode.crm
@@ -39,7 +33,7 @@ class BaseIntegrationTestCase extends Specification {
 
     Map getContentParams(Integer i) {
         return [
-            title: "Targeting Test $i Types and/or Phases", 
+            title: "Targeting Test $i Types and/or Phases",
             author: "Test User",
             subTitle: "To execute the JUnit integration test $i",
             body: "Grails organises tests by phase and by type. The state of the Grails application."]
